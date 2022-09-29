efc547d46cadfbc455b102e50a84ee9e5460269c
abecoup <abe.coup5@gmail.com>
2022-09-28 20:21:46 -0400

    added nat gateway, EIP, EC2 instance, comments

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+// VPC
 resource "aws_vpc" "hw4_vpc"{
 
     cidr_block = "10.0.0.0/24"
@@ -29,6 +30,7 @@ resource "aws_vpc" "hw4_vpc"{
 
 }
 
+// Internet Gateway
 resource "aws_internet_gateway" "hw4_igw"{
     vpc_id = aws_vpc.hw4_vpc.id
 
@@ -37,6 +39,7 @@ resource "aws_internet_gateway" "hw4_igw"{
     }
 }
 
+// Public Subnet 1
 resource "aws_subnet" "hw4-subnet1-public" {
     vpc_id = aws_vpc.hw4_vpc.id
     cidr_block = "10.0.0.0/26"
@@ -47,6 +50,7 @@ resource "aws_subnet" "hw4-subnet1-public" {
     }
 }
 
+// Public Subnet 2
 resource "aws_subnet" "hw4-subnet2-public" {
     vpc_id = aws_vpc.hw4_vpc.id
     cidr_block = "10.0.0.64/26"
@@ -57,6 +61,7 @@ resource "aws_subnet" "hw4-subnet2-public" {
     }
 }
 
+// Private Subnet 1
 resource "aws_subnet" "hw4-subnet1-private" {
     vpc_id = aws_vpc.hw4_vpc.id
     cidr_block = "10.0.0.128/26"
@@ -67,6 +72,7 @@ resource "aws_subnet" "hw4-subnet1-private" {
     }
 }
 
+// Private Subnet 2
 resource "aws_subnet" "hw4-subnet2-private" {
     vpc_id = aws_vpc.hw4_vpc.id
     cidr_block = "10.0.0.192/26"
@@ -77,6 +83,7 @@ resource "aws_subnet" "hw4-subnet2-private" {
     }
 }
 
+// Public Route Table
 resource "aws_route_table" "hw4-public-route-table" {
     vpc_id = aws_vpc.hw4_vpc.id
 
@@ -90,6 +97,7 @@ resource "aws_route_table" "hw4-public-route-table" {
     }
 }
 
+// Private Route Table
 resource "aws_route_table" "hw4-private-route-table" {
     vpc_id = aws_vpc.hw4_vpc.id
 
@@ -103,12 +111,52 @@ resource "aws_route_table" "hw4-private-route-table" {
     }
 }
 
+// Public Route Table Association
 resource "aws_route_table_association" "hw4-public-association" {
     subnet_id: aws_subnet.hw4-subnet1-public.id
     route_table_id: aws_route_table.hw4-public-route-table.id
 }
 
+// Private Route Table Association
 resource "aws_route_table_association" "hw4-private-association" {
     subnet_id: aws_subnet.hw4-subnet1-private.id
     route_table_id: aws_route_table.hw4-private-route-table.id
 }
+
+// NAT Gateway
+resource "aws_nat_gateway" "hw4-nat-gateway" {
+    allocation_id = aws_eip.hw4-elastic-ip.id
+    subnet_id     = aws_subnet.hw4-subnet1-public.id
+
+    tags = {
+        Name = "hw4-nat-gateway"
+    }   
+
+    // To ensure proper ordering
+    depends_on = [aws_internet_gateway.hw4_igw]
+}
+
+// Elastic IP
+resource "aws_eip" "hw4-elastic-ip" {
+    instance = aws_instance.hw4-ec2-instance.id
+    vpc      = true
+
+    depends_on = []
+}
+
+// EC2 Instance
+resource "aws_instance" "hw4-ec2-instance" {
+
+    // Amazon Linux 2 Kernel 5.10 AMI 2.0.20220912.1 x86_64 HVM gp2
+    ami           = "ami-026b57f3c383c2eec" 
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "hw4-ec2-instance"
+    }
+
+    // To ensure proper ordering
+    depends_on = [aws_internet_gateway.hw4_igw]
+}
+
+// need security group for EC2 here
